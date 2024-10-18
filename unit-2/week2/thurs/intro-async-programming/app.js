ffc50b9a4ad8f777faa06652c7d9a7808a5a30c7
mhurst <matthewhurst1791@gmail.com>
2024-10-17 20:05:16 -0400

    async

---
+++ unit-2/week2/thurs/intro-async-programming/app.js
@@ -0,0 +1,6 @@
+// async code means tasks can be run together
+// this can speed up run time
+// usually used where the user is waiting for things
+// async code is the reason "infinite scroll" can happen
+// users can interact with a small portion and then as they scroll more data can load
+// instead of loading all the informaiton which could take a long time
