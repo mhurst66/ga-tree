9334f0d0659f2fc8b237aa3a3d17135fb33d0e29
mhurst <matthewhurst1791@gmail.com>
2024-12-05 20:44:41 -0500

    react components lesson

---
+++ unit-3/reactComponents/react-components/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import Navbar from './components/Navbar/Navbar'
+import FounderListItem from './components/FounderListItem/FounderListItem'
+
+import './App.css'
+
+const App = () => {
+  const founders = [
+    {
+      name: "Elizabeth Holmes",
+      title: "CEO",
+      credential: "MBA from SuperLegit University",
+      id: 1,
+    },
+    {
+      name: "Sam Bankman-Fried",
+      title: "CFO",
+      credential: "CPA from TotallyReal State",
+      id: 2,
+    },
+    {
+      name: "Matt Damon",
+      title: "CMO",
+      credential: "Was in that movie you saw",
+      id: 3,
+    },
+  ];
+
+  return (
+    <>
+      <Navbar />
+      <main>
+        <h1>Welcome to Hyperbo.ly</h1>
+        <h2>The future is firmly in front of us!</h2>
+        <p>If you can't explain it to a fifth-grader, our product does it.</p>
+      </main>
+      <h2>Our Founders</h2>
+      <ul>
+        {founders.map((founder) => (
+          <FounderListItem key={founder.id} founder={founder} />
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default App
