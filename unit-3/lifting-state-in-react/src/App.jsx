70172b0f11798647927a3e7af5c1525371272b6a
mhurst <matthewhurst1791@gmail.com>
2024-12-16 18:22:01 -0500

    first example

--- unit-3/lifting-state-in-react/src/App.jsx
+++ unit-3/lifting-state-in-react/src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import IncrementButton from './components/IncrementButton';
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  const addOne = () => {
+    setCount(count + 1)
+  }
 
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Hello world!</h1>
+      <p>Count: {count}</p>
+      <IncrementButton addOne={addOne} />
+    </>
   );
 };
 
---
+++ unit-3/lifting-state-in-react/src/components/IncrementButton.jsx
@@ -0,0 +1,10 @@
+// src/components/IncrementButton.jsx
+
+const IncrementButton = (props) => {
+    return (
+      <button>Increment</button>
+    );
+};
+  
+export default IncrementButton;
+  
