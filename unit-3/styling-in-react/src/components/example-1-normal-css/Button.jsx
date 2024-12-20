161adf00ec7a1d02320f4fd09ac1f5ea77471df5
mhurst <matthewhurst1791@gmail.com>
2024-12-19 20:18:28 -0500

    styling react complete

---
+++ unit-3/styling-in-react/src/components/example-1-normal-css/Button.jsx
@@ -0,0 +1,8 @@
+import './Button.css'
+
+const Button = ({ buttonText }) => {
+    return <button className='btn'>{buttonText}</button>;
+  };
+  
+  export default Button;
+  
---
+++ unit-3/styling-in-react/src/components/example-3-css-in-js/Button.jsx
@@ -0,0 +1,21 @@
+import { createUseStyles } from "react-jss";
+
+const Button = ({ buttonText }) => {
+  const styles = createUseStyles({
+    btn: {
+      backgroundColor: '#24292e',
+      borderRadius: '8px',
+      color: '#f7df1c',
+      fontSize: '24pt',
+      padding: '15px 50px',
+      width: '300px',
+    }
+  })
+
+  const classes = styles()
+
+  return <button className={classes.btn}>{buttonText}</button>
+}
+  
+  export default Button;
+  
