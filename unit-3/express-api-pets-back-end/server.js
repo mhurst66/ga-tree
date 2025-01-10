a6aa7b88098c6871dc1b290df613d6a68fc95e59
mhurst <matthewhurst1791@gmail.com>
2025-01-09 19:47:25 -0500

    cors intro

--- unit-3/express-api-pets-back-end/server.js
+++ unit-3/express-api-pets-back-end/server.js
@@ -1,10 +1,17 @@
 const dotenv = require('dotenv')
 dotenv.config()
 const express = require('express')
+
+const cors = require('cors')
+
 const app = express()
 const mongoose = require('mongoose')
 
+
 const petRouter = require('./controllers/pets.js')
+
+app.use(cors())
+// app.use(cors({ origin: 'http://localhost:5173' }))
 
 mongoose.connect(process.env.MONGODB_URI)
 
