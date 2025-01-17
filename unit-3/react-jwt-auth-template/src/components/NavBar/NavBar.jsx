6d8534a6c0a87f7d4e8d6d59a1d241f3ab7a9dc3
mhurst <matthewhurst1791@gmail.com>
2025-01-16 21:01:51 -0500

    in progress

--- unit-3/react-jwt-auth-template/src/components/NavBar/NavBar.jsx
+++ unit-3/react-jwt-auth-template/src/components/NavBar/NavBar.jsx
@@ -1,11 +1,27 @@
-const NavBar = ({ user }) => {
+import { Link } from 'react-router-dom'
+
+const NavBar = ({ handleSignout, user }) => {
     return (
-      <>
-        <p>Hello Navbar</p>
-      </>
+        <>
+          { user ? (
+            <nav>
+              <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="" onClick={handleSignout}>Sign Out</Link></li>
+              </ul>
+            </nav>
+          ) : (
+            <nav>
+              <ul>
+                <li><Link to="/signin">Sign In</Link></li>
+                <li><Link to="/signup">Sign Up</Link></li>
+              </ul>
+            </nav>
+          )}
+        </>
     )
 }
 
 
-  export default NavBar
+export default NavBar
   
---
+++ unit-3/react-jwt-auth-template/src/services/authService.js
@@ -0,0 +1,76 @@
+const BACKEND_URL = 'http://localhost:3000'; // this is our Express API url
+
+const signup = async (formData) => {
+  try {
+    const res = await fetch(`${BACKEND_URL}/auth/sign-up`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData),
+    })
+    const json = await res.json()
+    if (json.token) {
+        localStorage.setItem('token', json.token); // add this line to store the JWT token in localStorage
+  
+        const user = JSON.parse(atob(json.token.split('.')[1]));
+  
+        return user
+    }
+    if (json.err) {
+      throw new Error(json.err)
+    }
+    return json
+  } catch (err) {
+    console.log(err)
+    throw err
+  }
+}
+
+const signin = async (user) => {
+    try {
+      const res = await fetch(`${BACKEND_URL}/users/signin`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(user),
+      });
+      const json = await res.json();
+
+      if (json.token) {
+        localStorage.setItem('token', json.token); // add this line to store the JWT token in localStorage
+  
+        const user = JSON.parse(atob(json.token.split('.')[1]));
+  
+        return user
+      }
+  
+      if (json.error) {
+        throw new Error(json.error);
+      }
+  
+      if (json.token) {
+        const user = JSON.parse(atob(json.token.split('.')[1]));
+        return user;
+      }
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+};
+
+const getUser = () =>  {
+    const token = localStorage.getItem('token');
+    if (!token) return null;
+    const user = JSON.parse(atob(token.split('.')[1]));
+    return user
+}
+
+const signout = () => {
+    localStorage.removeItem('token');
+};
+
+
+export {
+  signup,
+  signin,
+  getUser,
+  signout
+}
