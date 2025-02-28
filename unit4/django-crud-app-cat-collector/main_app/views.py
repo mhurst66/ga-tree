3e0c3923b1f4c4c14cd68fb7170358126df71ea7
mhurst <matthewhurst1791@gmail.com>
2025-02-27 20:04:56 -0500

    django build out

---
+++ unit4/django-crud-app-cat-collector/main_app/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+# Import HttpResponse to send text-based responses
+from django.http import HttpResponse
+
+class Cat:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+# Create a list of Cat instances
+cats = [
+    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
+    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
+    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
+]
+
+# Define the home view function
+def home(request):
+    # Send a simple HTML response
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def cat_index(request):
+    return render(request, 'cats/index.html', {'cats': cats})
