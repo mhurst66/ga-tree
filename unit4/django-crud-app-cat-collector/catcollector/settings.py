3d9e528683f5d796c6c97728e1863ea3b68ac11e
mhurst <matthewhurst1791@gmail.com>
2025-03-06 20:19:32 -0500

    cat collector complete

--- unit4/django-crud-app-cat-collector/catcollector/settings.py
+++ unit4/django-crud-app-cat-collector/catcollector/settings.py
@@ -118,6 +118,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGIN_URL = 'home'
+
+LOGIN_REDIRECT_URL = 'cat-index'
+
+LOGOUT_REDIRECT_ = 'home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- unit4/django-crud-app-cat-collector/main_app/urls.py
+++ unit4/django-crud-app-cat-collector/main_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.Home.as_view(), name='home'),
+    path('accounts/signup/', views.signup, name='signup'),
     path('about/', views.about, name='about'),
     path('cats/', views.cat_index, name='cat-index'),
     path('cats/<int:cat_id>/', views.cat_detail, name='cat-detail'),
@@ -14,4 +15,11 @@ urlpatterns = [
         views.add_feeding,
         name='add-feeding'
     ),
+    path('toys/create/', views.ToyCreate.as_view(), name='toy-create'),
+    path('toys/<int:pk>/', views.ToyDetail.as_view(), name='toy-detail'),
+    path('toys/', views.ToyList.as_view(), name='toy-index'),
+    path('toys/<int:pk>/update/', views.ToyUpdate.as_view(), name='toy-update'),
+    path('toys/<int:pk>/delete/', views.ToyDelete.as_view(), name='toy-delete'),
+    path('cats/<int:cat_id>/associate-toy/<int:toy_id>/', views.associate_toy, name='associate-toy'),
+    path('cats/<int:cat_id>/remove-toy/<int:toy_id>/', views.remove_toy, name='remove-toy'),
 ]
